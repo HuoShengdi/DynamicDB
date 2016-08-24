3655195d3b4be65e24fcfb456d7efb676d1c007c
Roscoe Huo <huoshengdi@gmail.com>
2016-08-24 04:29:27 -0700

    rename project for 'plausible deniability'

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/youplus.jsx",
+  entry: "./frontend/dynamicdb.jsx",
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
